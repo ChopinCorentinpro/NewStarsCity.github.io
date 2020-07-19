8547ceb773fc8b1eb0a67033d9df2ef051a24af2
CHOPIN Corentin <chopincorentin.pro@gmail.com>
2020-07-19 02:36:54 +0200

    Push status

---
+++ presset/page-all/footer/footer.php
@@ -0,0 +1,37 @@
+<div class="wrapper">
+  <footer class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h1 class="flipOutX">Spam Is Awesome! </h1>
+        <form class="form-inline">
+          <div class="form-group">
+            <input class="email" type="email" />
+            <button type="button" class="btn btn-default" >
+            <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+            </button>
+          </div>
+        </form> 
+        <p>We look forward to killing your inbox with annoying endelss spam!!!</p>
+      </div>
+      <div class="col-md-12">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+          <div class="row text-center ">
+            <div class="col-md-4 icon-container fadeInLeftBig"><i class="fa fa-facebook-official fa-3x" role="button"></i></div>
+            <div class="col-md-4"><i class="fa fa-linkedin fa-3x" role="button"></i></div>
+            <div class="col-md-4"><i class="fa fa-twitter fa-3x" role="button"></i></div>
+          </div>
+        </div>
+        <div class="col-md-4"></div>
+      </div>
+      <div class="col-md-12 footer-footer">
+        <div class="col-md-2"><p><a href="">Designed By Mark Grover</a></p></div>
+        <div class="col-md-2"></div>
+        <div class="col-md-2"></div>
+        <div class="col-md-2"></div>
+        <div class="col-md-2"><p><a href="">Contact</a></p></div>
+        <div class="col-md-2"><p><a href="">Customer Care</a></p></div>
+      </div>
+    </div>
+  </footer>
+</div>
